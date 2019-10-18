60009848b9b12728e5cbad8cc7f2b2c14dcba92d
Werner Weber <werner.weber@gmail.com>
2019-10-18 14:56:05 +0200

    Array Result Mapping Handler Fix

--- src/components/reactory/ReactoryFormComponent.js
+++ src/components/reactory/ReactoryFormComponent.js
@@ -524,12 +524,13 @@ class ReactoryComponent extends Component {
         const { data, loading, errors } = result;
         let _formData = formData;        
         if(data && data[query.name]) {          
+          debugger
           switch(query.resultType) {
-            case 'array' :{
+            case 'array' :{              
               let mergedData = []
               if(isArray(formData) === true) mergedData = [...formData];
               if(isArray(data[query.name]) === true) mergedData = [...mergedData, ...data[query.name]];
-              if(query.resultMap && Object.getOwnPropertyNames(query.resultTMap).length > 0 ) {
+              if(query.resultMap && Object.getOwnPropertyNames(query.resultMap).length > 0 ) {
                 _formData = objectMapper(mergedData, query.resultMap);
               } else {
                 _formData = mergedData;
