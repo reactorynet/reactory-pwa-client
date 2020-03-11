89656b1f849de09c8faec50233f03dae6c4717c0
Werner Weber <werner.weber@gmail.com>
2020-03-11 08:28:00 +0200

    Styled Currency List updated

--- src/components/shared/StyledCurrencyLabel.js
+++ src/components/shared/StyledCurrencyLabel.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { isArray } from 'lodash';
+import { isArray, indexOf } from 'lodash';
 import { compose } from 'recompose';
 import { withStyles, withTheme } from '@material-ui/core/styles';
 import { Tooltip } from '@material-ui/core';
@@ -9,7 +9,19 @@ import { withApi } from '@reactory/client-core/api/ApiProvider';
 class StyledCurrencyLabel extends Component {
 
   render() {
-    const { value, condition, currency, region, classes, uiSchema, currencies, displayAdditionalCurrencies } = this.props;
+    const { 
+      value, 
+      condition, 
+      currency, 
+      region, 
+      classes, 
+      uiSchema, 
+      currencies, 
+      displayAdditionalCurrencies = false, 
+      displayPrimaryCurrency = true,
+      currenciesDisplayed = null,
+      options = null, 
+    } = this.props;
 
     let isCents = true;
     let _value = value;
@@ -18,13 +30,17 @@ class StyledCurrencyLabel extends Component {
     let _containerProps = {};
     let _tooltip = '';
     
-    
+    let defaultStyle = {}
 
     if (uiSchema) {
       const uiOptions = uiSchema['ui:options'];
 
       isCents = uiOptions && uiOptions.isCents === false ? false : isCents;
       _value = uiOptions && (uiOptions.valueProp || this.props.formData) ? this.props[uiOptions.valueProp || 'formData'] : value;
+      
+      if(uiOptions.defaultStyle) defaultStyle = { ...uiOptions.defaultStyle };
+
+      _containerProps.style = { ...defaultStyle };
 
       if (uiOptions.prependText && uiOptions.prependText != '')
         _prependText = uiOptions.prependText;
@@ -35,7 +51,7 @@ class StyledCurrencyLabel extends Component {
       if (uiOptions.conditionalStyles && condition) {
         const matchingCondition = uiOptions.conditionalStyles.find(option => option.key === condition);
         if (matchingCondition) {
-          _containerProps.style = matchingCondition.style;
+          _containerProps.style = { ...defaultStyle, ...matchingCondition.style };
           if (matchingCondition.tooltip)
             _tooltip = matchingCondition.tooltip;
         }
@@ -45,28 +61,42 @@ class StyledCurrencyLabel extends Component {
     let otherCurrencies = [];
     
     if(currencies && isArray(currencies) && displayAdditionalCurrencies === true) {      
-      otherCurrencies = currencies.map((currency) => {
-        return (
-        <div className={classes.currency} {..._containerProps}>
-            <span>({currency.currency_code})&nbsp;</span>
-            <span className={classes.currencyValue}>
-              {new Intl.NumberFormat(region, { style: 'currency', currency: currency.currency_code }).format(isCents ? (currency.list_price_cents / 100) : currency.list_price_cents)}
-            </span>            
-        </div>)
+      currencies.forEach((currency) => {
+        let $add = true;
+        if(isArray(currenciesDisplayed) === true) {
+          
+          $add = indexOf(currenciesDisplayed, currency.currency_code) >= 0;
+        }
+
+        if($add === true) {
+          otherCurrencies.push((          
+            <div className={classes.currency} {..._containerProps}>
+                <span>({currency.currency_code})&nbsp;</span>
+                <span className={classes.currencyValue}>
+                  {new Intl.NumberFormat(region, { style: 'currency', currency: currency.currency_code }).format(isCents ? (currency.list_price_cents / 100) : currency.list_price_cents)}
+                </span>            
+            </div>
+          ))
+        }        
       });
     }
 
+    let primaryCurrency = (<div className={classes.currency} {..._containerProps}>
+      {_prependText != '' && <span>{_prependText}</span>}
+      <span className={classes.currencyValue}>
+        {new Intl.NumberFormat(region, { style: 'currency', currency }).format(isCents ? (_value / 100) : _value)}
+      </span>
+      {_postpendText != '' && <span>{_postpendText}</span>}    
+    </div>);
+    
+
+
     return (
       <Tooltip title={_tooltip} placement="right-start">
-        <div className={classes.currency} {..._containerProps}>
-          {_prependText != '' && <span>{_prependText}</span>}
-          <span className={classes.currencyValue}>
-            {new Intl.NumberFormat(region, { style: 'currency', currency }).format(isCents ? (_value / 100) : _value)}
-          </span>
-          {_postpendText != '' && <span>{_postpendText}</span>}
-          {otherCurrencies}
+        <div>
+        { displayPrimaryCurrency === true ? primaryCurrency :null }
+        { displayAdditionalCurrencies === true ? otherCurrencies : null }
         </div>
-        
       </Tooltip>
     );
   }
