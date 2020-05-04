2dfc9a16953a8aec755aca43f85158842b0bb1c1
DrewZa <drewmurphyza@gmail.com>
2020-05-04 12:14:09 +0200

    Scroll paging works

--- src/components/shared/GridLayoutComponent.js
+++ src/components/shared/GridLayoutComponent.js
@@ -3,13 +3,28 @@ import { compose } from 'recompose';
 import { withApi } from '@reactory/client-core/api/ApiProvider';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import { Grid } from '@material-ui/core';
+import {
+  Grid,
+  Typography,
+  Icon
+} from '@material-ui/core';
 import IntersectionVisible from 'react-intersection-visible';
 
 class GridLayoutWidget extends Component {
 
   static styles = (theme) => {
-    return {}
+    return {
+      loading: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2)
+      },
+      spinning: {
+        animation: 'spin 3s infinite',
+      },
+    }
   }
 
   state = {
@@ -17,13 +32,14 @@ class GridLayoutWidget extends Component {
     page: 0,
     pageSize: 10,
     totalCount: 0,
-    loadingData: true
+    loadingData: true,
+    variables: {}
   }
 
   constructor(props, context) {
     super(props, context);
 
-    this.componentDefs = props.api.getComponents(['core.Loading'])
+    this.componentDefs = props.api.getComponents(['core.Loading']);
   }
 
   getData = async () => {
@@ -34,20 +50,36 @@ class GridLayoutWidget extends Component {
 
     if (uiOptions.remoteData === true) {
       try {
+
+        console.log(this.state.page, '   ', this.state.pageSize, '   ', this.state.totalCount);
+
         this.setState({ loadingData: true });
+
         const graphqlDefinitions = formContext.$formState.formDef.graphql;
         if (graphqlDefinitions.query || graphqlDefinitions.queries) {
           let queryDefinition = graphqlDefinitions.query;
-          let variables = api.utils.objectMapper(self, uiOptions.variables);
-          api.log('GRIDLAYOUTWIDGET - Mapped variables for query', { variables }, 'debug');
-          const queryResult = await api.graphqlQuery(queryDefinition.text, variables).then();
 
+          let variables;
+          if (this.state.data.length == 0) {
+            variables = api.utils.objectMapper(self, uiOptions.variables);
+            this.setState({
+              page: variables.paging.page,
+              pageSize: variables.paging.pageSize,
+              variables: { ...variables }
+            });
+          } else {
+            variables = { ...this.state.variables, paging: { page: this.state.page, pageSize: this.state.pageSize } }
+          }
+
+          api.log('GRIDLAYOUTWIDGET - Mapped variables for query', { variables }, 'debug');
+
+          const queryResult = await api.graphqlQuery(queryDefinition.text, variables).then();
           if (queryResult.errors && queryResult.errors.length > 0) {
             api.log(`Error loading remote data for MaterialTableWidget`, { formContext, queryResult })
             this.setState({ data: [], page: 0, totalCount: 0, loadingData: false });
           } else {
             let result = api.utils.objectMapper(queryResult.data[queryDefinition.name], uiOptions.resultMap);
-            this.setState({ data: result.data, page: result.page - 1, totalCount: result.totalCount, loadingData: false });
+            this.setState((prevState) => ({ data: prevState.data.concat(result.data), page: result.page + 1, totalCount: result.totalCount, loadingData: false }));
           }
         } else {
           this.setState({ data: [], page: 0, totalCount: 0, loadingData: false });
@@ -72,31 +104,16 @@ class GridLayoutWidget extends Component {
   }
 
   onShow = (event) => {
-    console.log('______ONSHOW_____');
-
-    // load more data
-
-  }
-
-  onHide = (event) => {
-    console.log('______ONHIDE_____');
-
-    // not in use
-
-  }
-  onIntersect = (event) => {
-    console.log('______ONINTERSECT_____');
-
-    //
+    if (!this.state.loadingData) this.getData();
   }
 
   render() {
     const { props, state } = this;
-    const { api, classes, formData, formContext, uiSchema } = props;
+    const { api, classes, uiSchema } = props;
     const uiOptions = uiSchema["ui:options"] || {};
     const { Loading } = this.componentDefs;
 
-    const loadingMessage = uiOptions.leadingMessage ? uiOptions.leadingMessage : 'Loading Product Dimensions, please wait a moment';
+    const loadingMessage = uiOptions.loadingMessage ? uiOptions.loadingMessage : 'Loading Product Dimensions, please wait a moment';
 
     let ChildComponent = api.getComponent(uiOptions.component);
     let componentProps = {};
@@ -104,16 +121,21 @@ class GridLayoutWidget extends Component {
     return (
       <>
         <Grid container spacing={2} >
-          {state.loadingData && <Loading message={loadingMessage} />}
+          {state.loadingData && state.data.length == 0 && <Loading message={loadingMessage} />}
           {
-            state.data.map(itemData => {
+            state.data.map((itemData, index) => {
               componentProps = { data: itemData }
-              return (<Grid item xs={12} sm={6} md={3} ><ChildComponent {...componentProps} /></Grid>)
+              return (<Grid item xs={12} sm={6} md={3} key={index} ><ChildComponent {...componentProps} /></Grid>)
             })
           }
         </Grid>
-        {!state.loadingData && state.data.length > 0 && <IntersectionVisible onIntersect={this.onIntersect} onHide={this.onHide} onShow={this.onShow}>
-          <p>Loading more...</p>
+        {this.props.children}
+        {
+          state.loadingData && state.data.length > 0 &&
+          <Typography classes={{ root: classes.loading }} variant={'h6'} align="center">Loading more &nbsp;<Icon className={classes.spinning} color="primary">autorenew</Icon></Typography>
+        }
+        {!state.loadingData && state.data.length > 0 && <IntersectionVisible onShow={this.onShow}>
+          <Typography classes={{ root: classes.loading }} variant={'h6'}>Load More</Typography>
         </IntersectionVisible>}
       </>
     );
