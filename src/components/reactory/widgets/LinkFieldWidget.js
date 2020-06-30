8d949b9770feffab28ce2ae9eb5b5247cf8d7601
DrewZa <drewmurphyza@gmail.com>
2020-06-30 15:25:37 +0200

    Small change -  removing debuggers and adding conditional label to documentlist component

--- src/components/reactory/widgets/LinkFieldWidget.js
+++ src/components/reactory/widgets/LinkFieldWidget.js
@@ -13,46 +13,46 @@ class LinkFieldWidget extends Component {
 
     let linkText = template('/${formData}')({...props});
     let linkTitle = props.formData;
-    let linkIcon = null;          
+    let linkIcon = null;
     let _iconPosition = 'right';
     let theme = props.theme;
     let variant = "text";
     let _component = 'button';
-    let uioptions = props.uiSchema && props.uiSchema["ui:options"] ? props.uiSchema["ui:options"] : null;    
+    let uioptions = props.uiSchema && props.uiSchema["ui:options"] ? props.uiSchema["ui:options"] : null;
     if(uioptions){
-      const { 
-        format, 
-        title, 
-        icon, 
-        iconType, 
-        iconPosition, 
-        variant, 
+      const {
+        format,
+        title,
+        icon,
+        iconType,
+        iconPosition,
+        variant,
         iconProps = { },
-        component = 'button',        
+        component = 'button',
        } = uioptions;
-      if(format) linkText = template(format)(props)      
+      if(format) linkText = template(format)(props)
       if(title) linkTitle = template(title)(props)
       if(variant) _variant = variant
       if(iconPosition) _iconPosition = iconPosition;
        _component = component;
-      if(icon){        
-        const _iconProps = { 
-          styles: 
-          { 
-            marginLeft: _iconPosition === 'right' ? theme.spacing(1) : 'unset', 
+      if(icon){
+        const _iconProps = {
+          styles:
+          {
+            marginLeft: _iconPosition === 'right' ? theme.spacing(1) : 'unset',
             marginRight: _iconPosition === 'left' ? theme.spacing(1) : 'unset'
-          }, 
-          ...iconProps 
+          },
+          ...iconProps
         };
 
-        const _custom = iconType        
+        const _custom = iconType
         let IconComponent = _custom !== undefined ? theme.extensions[_custom].icons[icon] : null;
         if(IconComponent) {
           linkIcon = <IconComponent {...iconProps} />
         } else {
           linkIcon = <Icon {...iconProps}>{props.uiSchema["ui:options"].icon}</Icon>
         }
-      }                
+      }
     }
 
     if(uioptions === null) {
@@ -62,12 +62,10 @@ class LinkFieldWidget extends Component {
       }
     }
 
-    const goto = () => { 
+    const goto = () => {
       if(props.uiSchema["ui:options"].userouter === false) window.location.assign(linkText);
-      else props.history.push(linkText); 
+      else props.history.push(linkText);
     };
-
-    debugger;
 
     let $component = null;
     switch(_component.toLowerCase()){
