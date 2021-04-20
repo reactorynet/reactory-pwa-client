a6a821a495795d1a7ce1bf570d105a28a84ff319
Werner Weber <werner.weber@gmail.com>
2021-04-20 10:27:01 +0200

    Changed SliderWidget to tsx.

---
+++ src/components/reactory/widgets/SliderWidget.tsx
@@ -0,0 +1,60 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { throttle } from 'lodash';
+import {
+  FormControl,
+} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import Reactory from 'types/reactory';
+
+const styles = (theme) => ({
+  root: {
+    width: '85%',
+  },
+  slider: {
+    padding: '22px 0px',
+    margin: 'auto'
+  },
+});
+
+
+const SliderWidget = ({ formData, onChange, uiSchema, reactory }) => {
+
+  const handleChange = (event, value) => {
+    if (onChange) onChange(value);
+  };
+
+  let options: any = { min: 0, max: 100, step: 1, marks: true }
+  if (uiSchema['ui:options']) options = { ...options, ...uiSchema['ui:options'] }
+
+  /**
+   * 
+   *  getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+   * 
+   */
+
+  const getAriaValueText = (value: number) => {
+    return options.valueTextFormat ? reactory.utils.template(options.valueTextForm)({ value }) : `${value}`;
+  }
+
+  return (
+    <Slider
+      value={formData || 0}
+      min={options.min}
+      step={options.step || 1}
+      marks={options.marks === true}
+      max={options.max}
+      aria-labelledby="label"
+      onChange={handleChange}
+      getAriaValueText={getAriaValueText}
+      valueLabelDisplay={options.valueLabelDisplay || "auto"}
+    />
+  );
+}
+
+export const SliderWidgetComponent = withStyles(styles)(SliderWidget);
+export default SliderWidgetComponent
