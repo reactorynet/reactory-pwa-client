40ba6bbc41a6917eb5c9ccf932602b4f90d027ca
pharteveld <pharteveld@contractor.worldremit.com>
2025-01-29 06:29:29 +0200

    Fix for profile screen

--- src/components/reactory/ReactoryForm/hooks/useToolbar.tsx
+++ src/components/reactory/ReactoryForm/hooks/useToolbar.tsx
@@ -1,173 +1,241 @@
-
-
-// let icon = 'save';
-
-import { 
-  Button,
-  Icon,
-  Toolbar,
-  Tooltip, 
-} from "@mui/material";
-import { ReactoryFormToolbarHook } from "../types";
-import React, { useCallback } from "react";
+import { Button, Icon, Toolbar, Tooltip } from "@mui/material";
 import { useReactory } from "@reactory/client-core/api";
+import { template } from "lodash";
+import React, { useCallback } from "react";
+import { ReactoryFormToolbarHook } from "../types";
 
 /**
- * 
- * @param props 
+ *
+ * @param props
  */
-export const useToolbar: ReactoryFormToolbarHook = (props) => { 
+export const useToolbar: ReactoryFormToolbarHook = (props) => {
+  const {
+    formDefinition,
+    uiOptions,
+    onSubmit,
+    SubmitButton,
+    formData,
+    errorSchema,
+    errors,
+    SchemaSelector,
+    refresh,
+    toggleHelp,
+  } = props;
 
-const {
-  formDefinition,
-  uiOptions,
-  onSubmit,
-  SubmitButton,
-  formData,
-  errorSchema,
-  errors,
-  SchemaSelector,
-  refresh,
-  toggleHelp,
-} = props;
+  const reactory = useReactory();
 
-const reactory = useReactory();
-
-let icon = 'save';
+  let icon = "save";
 
   // BACKWARDS COMPATIBLE SCHEMA OBJECTS
-if (typeof formDefinition.uiSchema === "object") {
-  if (formDefinition.uiSchema && formDefinition.uiSchema.submitIcon) {
-    if (typeof formDefinition.uiSchema.submitIcon === 'string') {
-      icon = formDefinition.uiSchema.submitIcon
+  if (typeof formDefinition.uiSchema === "object") {
+    if (formDefinition.uiSchema && formDefinition.uiSchema.submitIcon) {
+      if (typeof formDefinition.uiSchema.submitIcon === "string") {
+        icon = formDefinition.uiSchema.submitIcon;
+      }
+    }
+
+    if (
+      formDefinition.uiSchema &&
+      formDefinition.uiSchema["ui:options"] !== null &&
+      typeof formDefinition.uiSchema["ui:options"] === "object"
+    ) {
+      if ((formDefinition.uiSchema["ui:options"] as any).submitIcon) {
+        icon = (formDefinition.uiSchema["ui:options"] as any).submitIcon;
+      }
+    }
+
+    if (
+      formDefinition.uiSchema &&
+      formDefinition.uiSchema["ui:form"] !== null &&
+      typeof formDefinition.uiSchema["ui:form"] === "object"
+    ) {
+      if ((formDefinition.uiSchema["ui:form"] as any).submitIcon) {
+        icon = (formDefinition.uiSchema["ui:form"] as any).submitIcon;
+      }
     }
   }
 
-  if (formDefinition.uiSchema && formDefinition.uiSchema['ui:options'] !== null
-    && typeof formDefinition.uiSchema['ui:options'] === 'object') {
-    if ((formDefinition.uiSchema['ui:options'] as any).submitIcon) {
-      icon = (formDefinition.uiSchema['ui:options'] as any).submitIcon
-    }
-  }
+  if (uiOptions?.submitIcon) icon = uiOptions.submitIcon;
+  let iconProps = uiOptions?.submitIconProps || {};
+  let iconWidget =
+    icon === "$none" ? null : React.createElement(Icon, iconProps, icon);
+  let showSubmit = true;
+  let showRefresh = true;
+  let showHelp = true;
+  let submitButton = null;
 
-  if (formDefinition.uiSchema && formDefinition.uiSchema['ui:form'] !== null
-    && typeof formDefinition.uiSchema['ui:form'] === 'object') {
-    if ((formDefinition.uiSchema['ui:form'] as any).submitIcon) {
-      icon = (formDefinition.uiSchema['ui:form'] as any).submitIcon
-    }
-  }
-}
+  let submitTooltip = "Click to submit the form";
 
-if (uiOptions?.submitIcon) icon = uiOptions.submitIcon;
-let iconProps = uiOptions?.submitIconProps || {};
-let iconWidget = (icon === '$none' ? null : <Icon {...iconProps}>{icon}</Icon>);
-let showSubmit = true;
-let showRefresh = true;
-let showHelp = true;
-let submitButton = null;
+  const { submitProps, buttons } = uiOptions;
+  if (typeof submitProps === "object" && showSubmit === true) {
+    const {
+      variant = "fab",
+      iconAlign = "left",
+      tooltip = submitTooltip,
+    } = submitProps;
+    const _props = { ...submitProps };
+    delete _props.iconAlign;
+    _props.onClick = useCallback(() => {
+      onSubmit(formData);
+    }, [onSubmit, formData, errorSchema, errors]);
 
-let submitTooltip = 'Click to submit the form';
+    submitTooltip = reactory.utils.template(tooltip as string)({
+      props: props,
+      state: { formData },
+    });
 
-const { submitProps, buttons } = uiOptions;
-if (typeof submitProps === 'object' && showSubmit === true) {
-  const { variant = 'fab', iconAlign = 'left', tooltip = submitTooltip } = submitProps;
-  const _props = { ...submitProps };
-  delete _props.iconAlign;
-  _props.onClick = useCallback(() => { onSubmit(formData) }, [onSubmit, formData, errorSchema, errors]);
-
-  submitTooltip = reactory.utils.template(tooltip as string)({
-    props: props,
-    state: { formData }
-  });
-
-
-  if (variant && typeof variant === 'string' && showSubmit === true) {
-    switch (variant) {
-      case 'fab':
-        {
+    if (variant && typeof variant === "string" && showSubmit === true) {
+      switch (variant) {
+        case "fab": {
           delete _props.variant;
           //@ts-ignore
-          submitButton = (<Fab {..._props}>{iconWidget}</Fab>);
+          submitButton = <Fab {..._props}>{iconWidget}</Fab>;
           break;
         }
-      default: {
-        //@ts-ignore
-        submitButton = (<Button {..._props}>{iconAlign === 'left' && iconWidget}{template(_props.text)({ props: props, formData, formDef: formDefinition, reactory })}{iconAlign === 'right' && iconWidget}</Button>);
-        break;
+        default: {
+          //@ts-ignore
+          submitButton = (
+            <Button
+              {..._props}
+              variant={
+                ["text", "contained", "outlined"].includes(
+                  _props.variant as string
+                )
+                  ? (_props.variant as "text" | "contained" | "outlined")
+                  : undefined
+              }
+              href={typeof _props.href === "string" ? _props.href : undefined}
+            >
+              {iconAlign === "left" && iconWidget}
+              {typeof _props.text === "string" &&
+                template(_props.text)({
+                  props: props,
+                  formData,
+                  formDef: formDefinition,
+                  reactory,
+                })}
+              {iconAlign === "right" && iconWidget}
+            </Button>
+          );
+          break;
+        }
       }
     }
   }
-}
 
-if (uiOptions && reactory.utils.lodash.isNil(uiOptions.showSubmit) === false) {
-  showSubmit = uiOptions.showSubmit === true;
-}
-
-if (uiOptions && reactory.utils.lodash.isNil(uiOptions.showHelp) === false) {
-  showHelp = uiOptions.showHelp === true;
-}
-
-if (uiOptions && reactory.utils.lodash.isNil(uiOptions.showRefresh) === false) {
-  showRefresh = uiOptions.showRefresh === true;
-}
-
-let additionalButtons = [];
-if (buttons && buttons.length) {
-  additionalButtons = buttons.map((button: any, buttonIndex) => {
-    const { buttonProps, iconProps, type, handler, component } = button;
-
-    if (component && typeof component === "function") return component;
-
-    const onButtonClicked = () => {
-      reactory.log(`OnClickButtonFor Additional Buttons`);
-      if (props[handler] && typeof props[handler] === 'function') {
-        (props[handler] as Function)({ reactoryForm: this, button })
-      } else {
-        reactory.createNotification(`No handler '${handler}' for ${buttonProps.title} button`, { showInAppNotification: true, type: 'error' })
-      }
-    }
-
-    let buttonIcon = null;
-    if (iconProps) {
-      buttonIcon = <Icon {...iconProps}>{iconProps.icon}</Icon>
-    }
-
-    return (
-      <Button {...buttonProps} key={buttonIndex} onClick={onButtonClicked}>{iconProps.placement === 'left' && buttonIcon}{buttonProps.title}{iconProps.placement === 'right' && buttonIcon}</Button>
-    )
-  });
-}
-
-const RefreshButton = () => {
-  if (showRefresh === true) {
-    return (
-      <Button onClick={() => { refresh() }} variant="text" color="primary">
-        <Icon>refresh</Icon>
-      </Button>
-    )
+  if (
+    uiOptions &&
+    reactory.utils.lodash.isNil(uiOptions.showSubmit) === false
+  ) {
+    showSubmit = uiOptions.showSubmit === true;
   }
 
-  return null;
-}
+  if (uiOptions && reactory.utils.lodash.isNil(uiOptions.showHelp) === false) {
+    showHelp = uiOptions.showHelp === true;
+  }
 
-const FormToolbar = () => (
-  <Toolbar style={uiOptions.toolbarStyle || {}}>
-    {uiOptions.showSchemaSelectorInToolbar && 
-    !uiOptions.showSchemaSelectorInToolbar === false &&
-    SchemaSelector ? <SchemaSelector /> : null}
-    {showSubmit === true && SubmitButton ? (<Tooltip title={submitTooltip}><SubmitButton /></Tooltip>) : null}
-    {additionalButtons}
-    {<RefreshButton />}        
-    {formDefinition.backButton && <Button variant="text" onClick={() => { history.back() }} color="primary">BACK <Icon>keyboard_arrow_left</Icon></Button>}
-    {formDefinition.helpTopics && showHelp === true && <Button variant="text" onClick={() => { toggleHelp() }} color="primary"><Icon>help</Icon></Button>}
-  </Toolbar>
-);
+  if (
+    uiOptions &&
+    reactory.utils.lodash.isNil(uiOptions.showRefresh) === false
+  ) {
+    showRefresh = uiOptions.showRefresh === true;
+  }
 
-let toolbarPosition = uiOptions.toolbarPosition || 'bottom'
+  let additionalButtons = [];
+  if (buttons && buttons.length) {
+    additionalButtons = buttons.map((button: any, buttonIndex) => {
+      const { buttonProps, iconProps, type, handler, component } = button;
 
+      if (component && typeof component === "function") return component;
+
+      const onButtonClicked = () => {
+        reactory.log(`OnClickButtonFor Additional Buttons`);
+        if (props[handler] && typeof props[handler] === "function") {
+          (props[handler] as Function)({ reactoryForm: this, button });
+        } else {
+          reactory.createNotification(
+            `No handler '${handler}' for ${buttonProps.title} button`,
+            { showInAppNotification: true, type: "error" }
+          );
+        }
+      };
+
+      let buttonIcon = null;
+      if (iconProps) {
+        buttonIcon = <Icon {...iconProps}>{iconProps.icon}</Icon>;
+      }
+
+      return (
+        <Button {...buttonProps} key={buttonIndex} onClick={onButtonClicked}>
+          {iconProps.placement === "left" && buttonIcon}
+          {buttonProps.title}
+          {iconProps.placement === "right" && buttonIcon}
+        </Button>
+      );
+    });
+  }
+
+  const RefreshButton = () => {
+    if (showRefresh === true) {
+      return (
+        <Button
+          onClick={() => {
+            refresh();
+          }}
+          variant="text"
+          color="primary"
+        >
+          <Icon>refresh</Icon>
+        </Button>
+      );
+    }
+
+    return null;
+  };
+
+  const FormToolbar = () => (
+    <Toolbar style={uiOptions.toolbarStyle || {}}>
+      {uiOptions.showSchemaSelectorInToolbar &&
+      !uiOptions.showSchemaSelectorInToolbar === false &&
+      SchemaSelector ? (
+        <SchemaSelector />
+      ) : null}
+      {showSubmit === true && SubmitButton ? (
+        <Tooltip title={submitTooltip}>
+          <SubmitButton />
+        </Tooltip>
+      ) : null}
+      {additionalButtons}
+      {<RefreshButton />}
+      {formDefinition.backButton && (
+        <Button
+          variant="text"
+          onClick={() => {
+            history.back();
+          }}
+          color="primary"
+        >
+          BACK <Icon>keyboard_arrow_left</Icon>
+        </Button>
+      )}
+      {formDefinition.helpTopics && showHelp === true && (
+        <Button
+          variant="text"
+          onClick={() => {
+            toggleHelp();
+          }}
+          color="primary"
+        >
+          <Icon>help</Icon>
+        </Button>
+      )}
+    </Toolbar>
+  );
+
+  let toolbarPosition = uiOptions.toolbarPosition || "bottom";
 
   return {
     Toolbar: FormToolbar,
-    toolbarPosition
-  }
+    toolbarPosition,
+  };
 };
