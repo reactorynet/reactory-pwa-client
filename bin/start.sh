2153b56cb2b8bf338fa3b63b0f3f924d3126fd92
Werner Weber <wweber@worldremit.com>
2025-03-14 02:03:59 +0200

    RSE-11: Refactor build scripts to improve environment variable handling and add image export functionality

--- bin/start.sh
+++ bin/start.sh
@@ -13,6 +13,8 @@
 
 # # Get additional options from the command line arguments
 additional_options="${@:3}"
+project_key=${1:-reactory}
+target=${2:-local}
 
 echo "Starting Reactory Web Client key: [$project_key] target: $target with additional options: $additional_options"
-env-cmd -f ./config/env/${1:-reactory}/.env.${2:-local} npx babel-node scripts/start.ts --presets @babel/env --extensions ".js,.ts" --max_old_space_size=2000000
+env-cmd -f ./config/env/$project_key/.env.$target npx babel-node scripts/start.ts --presets @babel/env --extensions ".js,.ts" --max_old_space_size=2000000
